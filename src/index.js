8eb452e512b58a9a646c359845fb3182dbeaed40
keerthi vardhan <keerthivardhan2k@gmail.com>
2025-09-30 09:56:30 +0530

    Setup Mongo Connection

---
+++ src/index.js
@@ -0,0 +1,27 @@
+// require('dotenv').config({path: "./env"})
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+dotenv.config({
+  path: "./env",
+});
+
+connectDB();
+
+// const app = express();
+
+// (async () => {
+//   try {
+//     await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}}`);
+//     app.on("error", (error) => {
+//       console.log(error)
+//     })
+
+//     app.listen(process.env.PORT, () => {
+//       console.log("App is listening on port ", process.env.PORT)
+//     })
+
+//   } catch (error) {
+//     console.log(`Error: ${error}`);
+//     throw
+//   }
+// })();
